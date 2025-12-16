0def085171014cf031d7db66ffd4832502397979
alihasna32 <muktermukter555@gmail.com>
2025-12-16 18:13:48 +0600

    file setup

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 3000;
+
+const app = express();
+// middleware
+app.use(
+  cors({
+    // origin:"loanlink-project-323.netlify.app",
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const db = client.db("LoanLink-server");
+    const loanCollection = db.collection("loans");
+    const applicationCollection = db.collection("loanApplications");
+    const usersCollection = db.collection("users");
+
+  } catch (err) {
+    console.log(err);
+  } finally {
+    // await client.close();
+  }
+}
+  run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("loanlink server is running..");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  if (!process.env.STRIPE_SECRET_KEY) console.warn("WARNING: STRIPE_SECRET_KEY is not set in .env");
+  if (!process.env.CLIENT_DOMAIN) console.warn("WARNING: CLIENT_DOMAIN is not set in .env");
+});
